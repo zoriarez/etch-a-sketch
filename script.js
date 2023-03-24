4a6b582f29544e51401d7aa4ae5e1c69d880f3e5
zoriarez <127724840+zoriarez@users.noreply.github.com>
2023-03-24 20:19:15 +0800

    adjustable canvas range

---
+++ script.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById("Canvas");
+const rangeText = document.getElementById("rangeText");
+const pixelsRange = document.getElementById("pixelsRange");
+
+let pixelQuantity;
+let pixel;
+
+pixelsRange.addEventListener('input',()=>{
+    console.log(pixelsRange.value)
+    pixelQuantity= pixelsRange.value * pixelsRange.value;
+    remove(canvas)
+    for(i=0; i < pixelQuantity; i++){
+        pixel = document.createElement("div");
+        pixel.classList.add('pixels')
+
+        canvas.style.gridTemplateColumns = `repeat(${pixelsRange.value},auto)`
+        canvas.appendChild(pixel);
+    }
+})
+
+function remove(a){
+    while (a.firstChild) {
+        a.removeChild(a.firstChild);
+}}
